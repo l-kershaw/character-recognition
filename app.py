b2383432e29dec043fb15ee3c25afe15da368640
l-kershaw <35707277+l-kershaw@users.noreply.github.com>
2020-10-24 17:40:31 +0100

    Add basic canvas

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash_canvas import DashCanvas
+
 app = dash.Dash(__name__)
 server = app.server
+
+canvas_width = 300
+bg_filename = "https://l-kershaw.github.io/images/border.jpg"
+
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(
@@ -11,7 +17,15 @@ app.layout = html.Div([
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
         value='LA'
     ),
-    html.Div(id='display-value')
+    html.Div(id='display-value'),
+		DashCanvas(	id='canvas',
+								tool='pencil',
+								width=canvas_width,
+								height=1000,
+								lineColor='black',
+								filename=bg_filename,
+								hide_buttons=['pencil','line','zoom','pan','rectangle','select'],#,'undo','redo'],
+								goButtonTitle='Process')
 ])
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
